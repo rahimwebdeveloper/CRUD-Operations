3fb9805b501a6a96bbf29b0e4dd6e54bda41b9f8
rahimwebdeveloper <info.abdurrahim12@gmail.com>
2022-10-13 12:27:58 +0600

    sow data random user

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,12 +1,13 @@
 const data = require('../data.json')
 
 module.exports.getRandomUser = (req, res) => {
-    
+    const number = Math.floor(Math.random() * 10);
+    const user = data.find(user => user.id == number);
+    res.send(user);
+
 }
 module.exports.getAllUser = (req, res) => {
-    console.log(data)
-    res.send(data)
-
+    res.send(data);
 }
 module.exports.postUser = (req, res) => {
 
